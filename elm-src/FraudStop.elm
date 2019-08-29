b38b579eb3c19cd31c2c02b686730808fac57b40
Tim McEwan <tim@mcewan.it>
2019-08-29 19:07:05 +1000

    Poorly modelled & buggy accordion toggling

--- elm-src/FraudStop.elm
+++ elm-src/FraudStop.elm
@@ -1,44 +1,90 @@
 module FraudStop exposing (main)
 
+import Browser exposing (element)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 type alias Accordion =
-    { header : String
+    { open : Bool
+    , header : String
     , content : String
     }
+
+
+type alias Model =
+    { accordions : List Accordion
+    }
+
+
+initialAccordions : List Accordion
+initialAccordions =
+    [ { open = False, header = "How does it work?", content = "Lorem ipsum" }
+    , { open = False, header = "Why did we build Fraudstop?", content = "Lorem ipsum" }
+    , { open = False, header = "Some very important legal information.", content = "Lorem ipsum" }
+    ]
+
+
+initialModel : Model
+initialModel =
+    { accordions = initialAccordions
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type Msg
+    = ClickedAccordion Accordion
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedAccordion accordion ->
+            ( { model | accordions = { accordion | open = not accordion.open } :: model.accordions }, Cmd.none )
 
 
 
 -- MAIN
 
 
-accordions : List Accordion
-accordions =
-    [ { header = "How does it work?", content = "Lorem ipsum" }
-    , { header = "Why did we build Fraudstop?", content = "Lorem ipsum" }
-    , { header = "Some very important legal information.", content = "Lorem ipsum" }
-    ]
-
-
-
--- accordionView : accordions -> Html
-
-
+accordionView : Accordion -> Html Msg
 accordionView accordion =
+    let
+        closedClass =
+            if accordion.open then
+                ""
+
+            else
+                " accordion--closed"
+    in
     section [ class "accordion header-accordion" ]
-        [ button [ class "accordion-title" ]
+        [ button [ onClick <| ClickedAccordion accordion, class ("accordion-title" ++ closedClass) ]
             [ img [ src "/static/images/orangeArrow.svg", class "accordion-arrow" ] []
             , text accordion.header
             ]
-        , div [ class "accordion-body" ]
-            [ text accordion.content
+        , div [ class ("accordion-body" ++ closedClass) ]
+            [ p [] [ text accordion.content ]
             ]
         ]
 
 
-main =
+view : Model -> Html Msg
+view model =
     div []
         [ div [ class "header-container" ]
             [ div [ class "grid-container" ]
@@ -49,7 +95,7 @@ main =
                             ]
                         , h2 [ class "h5 mb-4" ]
                             [ text "FraudStop makes it quick and easy to appeal an automated debt claim against you. All you need to do is enter a few details, explain why you want to appeal the debt claim against you, and hit send - FraudStop does the rest." ]
-                        , div [] <| List.map accordionView accordions
+                        , div [] <| List.map accordionView model.accordions
                         ]
                     ]
                 ]
