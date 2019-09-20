b1f7e119f8c396e6b7d0fba7eb5aa34ed2f24f37
Tim McEwan <tim@mcewan.it>
2019-09-21 00:26:51 +1000

    Pass page's query string into Elm

--- pages/fraudstop/verify.js
+++ pages/fraudstop/verify.js
@@ -3,8 +3,10 @@ import Elm from 'react-elm-components'
 import Layout from '../../components/Layout'
 import Verify from '../../elm-src/FraudStopVerify.elm'
 
+const query = process.browser && window.location.search
+
 export default () => (
   <Layout>
-    <Elm src={Verify.Elm.FraudStopVerify} />
+    <Elm src={Verify.Elm.FraudStopVerify} flags={{ query }} />
   </Layout>
 )
